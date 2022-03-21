90ef7cbf97d146a6f956c44c6df594fdd2dd06a2
Hudson5697 <80768404+Hudson5697@users.noreply.github.com>
2022-03-21 21:04:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var random1=Math.floor(Math.random()*6)+1;
 var random2=Math.floor(Math.random()*6)+1;
 var image="dice"+ random1 + ".png";
-var img="images/"+ image;
+var img= image;
 var image1="dice"+ random2 + ".png";
-var img1="images/"+ image1;
+var img1=image1;
 var s1=document.querySelectorAll("img")[0];
 s1=s1.setAttribute("src",img);
 var s2=document.querySelectorAll("img")[1];
